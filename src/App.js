411c6babb5669927d00681c0640f3baa92c7f228
Igor <igor.tsimbalyuk5@gmail.com>
2020-11-24 20:23:30 +0200

    5 урок по React Pizza окончен

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 import './scss/app.scss';
 
@@ -7,11 +8,19 @@ import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  });
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import classNames from 'classnames'
+import React from 'react';
+import classNames from 'classnames';
 
-const Button = ({children, className, outline}) => {
-    return (
-        <button  className={classNames('button', className, {
-            'button--outline': outline    
-        })}>{children}</button>
-    )
-}
+const Button = ({ children, className, outline }) => {
+  return (
+    <button
+      className={classNames('button', className, {
+        'button--outline': outline,
+      })}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
